72e67a744f95eff128b7f2427fd1d1d3f81f7a25
Equinox86 <Scarras86@gmail.com>
2020-03-07 17:41:58 -0500

    Dir

---
+++ laser_cat_ams7/laser_cat_ams7/main.c
@@ -0,0 +1,18 @@
+/*
+ * laser_cat_ams7.c
+ *
+ * Created: 3/7/2020 5:34:22 PM
+ * Author : Equinox
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
--- lasercat.ino
+++ lasercat.ino
@@ -4,33 +4,38 @@ Servo laser_head;
 #define MAX_ANGLE 120
 int lpos =0;
 int sweep_time = 15; 
+
 void setup() {
   // put your setup code here, to run once:
-laser_head.attach(9);
-Serial.begin(9600);
-pinMode(5,OUTPUT);
-randomSeed(analogRead(0));
-
+  laser_head.attach(9);
+  Serial.begin(9600);
+  pinMode(5,OUTPUT);
+  digitalWrite(5,LOW);
+  randomSeed(analogRead(0));
 }
 
 void run_sweep(unsigned long stop_time) { 
   //Turn on the Laser
   digitalWrite(5,HIGH);
+  unsigned long counter =0;
 
-  while(millis() < stop_time) {
+  while(counter < stop_time) {
+    unsigned long strt_time = millis();
     sweep_time= random(10)+5;
     for(lpos=0; lpos <= MAX_ANGLE; lpos++){ 
       laser_head.write(lpos);
       delay(sweep_time); 
       Serial.println(lpos);
-      printf(lpos);
+      //printf(lpos);
     }
     sweep_time= random(10)+5;
     for(lpos=MAX_ANGLE; lpos >=0;lpos--){ 
       laser_head.write(lpos);
-      Serial.println(lpos);
+      //Serial.println(lpos);
       delay(sweep_time); 
     }
+    unsigned long add_time = millis() - strt_time;
+    counter += add_time;
   }  
 
   //Turn laser off 
@@ -41,5 +46,5 @@ void loop() {
   //Set the next start time; 
   unsigned long next_start = 10000; //10 second test
   delay(next_start); 
-  run_sweep(millis() + (1000 * 180)); // Run for 3 minutes
+  run_sweep(1000 * 180); // Run for 3 minutes
 }
